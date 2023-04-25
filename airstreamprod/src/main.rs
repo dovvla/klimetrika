1b5ddeb15f1df0407d8a8d08be0243a6ba3b7027
Vladimir Jovin <vladimirjovin8@gmail.com>
2023-04-25 16:25:16 +0200

    rewrite kafka producer

--- airstreamprod/src/main.rs
+++ airstreamprod/src/main.rs
@@ -1,20 +1,22 @@
+use std::collections::VecDeque;
+use std::sync::Arc;
+
 use kafka::producer::{Producer, Record, RequiredAcks};
+use tokio::sync::RwLock;
 use tokio::task::JoinSet;
 use tokio::time::{sleep, Duration};
 
-fn get_time_from_reading(reading: &String) -> Option<String> {
+fn get_time_from_reading(reading: &str) -> Option<String> {
     let read_values: Vec<String> = reading
-        .split("\n")
+        .split('\n')
         .into_iter()
         .map(|x| x.to_string())
         .collect();
-    match read_values.get(1) {
-        Some(row) => Some(row.split_once("|").unwrap().0.to_string()),
-        None => None,
-    }
+    read_values
+        .get(1)
+        .map(|row| row.split_once('|').unwrap().0.to_string())
 }
 
-#[allow(unused)]
 #[tokio::main]
 async fn main() {
     let devices = reqwest::get("https://devel.klimerko.org/api/devices")
@@ -24,25 +26,42 @@ async fn main() {
         .await
         .expect("resposne body");
     let devices: Vec<String> = devices
-        .split("\n")
+        .split('\n')
         .skip(1)
-        .filter(|device| device.contains("|"))
-        .map(|device| device.split_once("|").unwrap().0.to_string())
+        .filter(|device| device.contains('|'))
+        .map(|device| device.split_once('|').unwrap().0.to_string())
         .collect();
 
     let mut set = JoinSet::new();
 
+    let message_queue = Arc::new(RwLock::new(VecDeque::<String>::new()));
+    let publish_queue = message_queue.clone();
+
+    set.spawn(async move {
+        let mut producer = Producer::from_hosts(vec!["kafka:29092".to_owned()])
+            .with_ack_timeout(Duration::from_secs(1))
+            .with_required_acks(RequiredAcks::One)
+            .create()
+            .expect("kafka connection");
+        loop {
+            let mut queue = publish_queue.write().await;
+            if let Some(reading) = (*queue).pop_front() {
+                producer
+                    .send(&Record::from_value("test-topic", reading.as_bytes()))
+                    .unwrap();
+                println!("{reading:} published!");
+            };
+            sleep(Duration::from_secs(2)).await;
+        }
+    });
+
     for device in devices {
+        let queue = message_queue.clone();
         set.spawn(async move {
             let mut previous_time: Option<String> = None;
-            let mut producer = Producer::from_hosts(vec!["localhost:9092".to_owned()])
-                .with_ack_timeout(Duration::from_secs(1))
-                .with_required_acks(RequiredAcks::One)
-                .create()
-                .expect("kafka connection");
             let client = reqwest::Client::new();
             loop {
-                sleep(Duration::from_millis(10000)).await;
+                sleep(Duration::from_millis(1000)).await;
                 let reading = client
                     .get(format!(
                         "https://devel.klimerko.org/api/device/{device:}/latest"
@@ -52,32 +71,27 @@ async fn main() {
                     .expect("klimerko unreachable")
                     .text()
                     .await
-                    .expect("resposne body");
+                    .expect("response body");
                 let current_time = get_time_from_reading(&reading);
+
                 match previous_time {
-                    Some(ref pt) => match current_time {
-                        Some(ct) => {
+                    Some(ref pt) => {
+                        if let Some(ct) = current_time {
                             if !pt.eq(&ct) {
                                 previous_time = Some(ct.to_string());
-                                producer
-                                    .send(&Record::from_value("my-topic", reading.as_bytes()))
-                                    .unwrap();
+                                queue.write().await.push_front(reading);
                             }
                         }
-                        None => {}
-                    },
-                    None => match current_time {
-                        Some(ct) => {
+                    }
+                    None => {
+                        if let Some(ct) = current_time {
                             previous_time = Some(ct.to_string());
-                            producer
-                                .send(&Record::from_value("my-topic", reading.as_bytes()))
-                                .unwrap();
+                            queue.write().await.push_front(reading);
                         }
-                        None => {}
-                    },
+                    }
                 }
             }
         });
     }
-    while let Some(res) = set.join_next().await {}
+    while let Some(_res) = set.join_next().await {}
 }
